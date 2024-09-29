d3d2ee11cdf3b6ad6107b72b476d153e4f2dbad8
albert19_ <albertnanda1902@gmail.com>
2024-09-29 20:17:25 +0800

    test: create testing endpoint to see if its works

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('api/v1', function($routes) {
+    $routes->get('test', 'Home::test');
+});
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,17 @@
 <?php
 
 namespace App\Controllers;
+use CodeIgniter\API\ResponseTrait;
 
 class Home extends BaseController
 {
     public function index(): string
     {
         return view('welcome_message');
+    }
+
+    public function test()
+    {
+        return $this->response->setJSON(['message' => 'Hello, World!']);
     }
 }
